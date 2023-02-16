0aaa2e3b9bee0003a222bf9cea5704f7c3784f23
Jonathon E. Corrales de Oliveira <jcjonathonc5@gmail.com>
2023-02-15 19:08:50 -0800

    tuesday night lights

--- monday_night_strat.py
+++ monday_night_strat.py
@@ -1,7 +1,11 @@
 import argparse
 from strategies import *
 from utils import *
+from optimized_sim import sim_1v1
+import concurrent
 
+tested = set()
+winners = {}
 
 def main():
     # Setup Graph
@@ -17,12 +21,60 @@ def main():
     if splitname[0][-1] == "J":
         res = label_prop_jungle_strat(G, num_seeds)
     else:
-        res = super_secret_strategy(G, num_seeds)
+        res = monkey_monkey(file_name, 4, 4)
 
     print(res)
 
     output_seeds(res, file_name)
     
+
+def monkey_monkey(file_name, num_workers, mins=4):
+    print(mins)
+    splitname = file_name.split('.')
+    num_seeds = int(splitname[1])
+
+    # Setup the graph.
+    G, idx_to_label = construct_graph(file_name)
+    diag = 1.5 * np.ones(G.order())
+    A = nx.adjacency_matrix(G) + np.diag(diag)
+
+    # Use degree centrality as a baseline.
+    c_deg = nx.degree_centrality(G)
+    sorted_deg = sorted(c_deg.keys(), key=lambda x : c_deg[x], reverse=True)
+    seeds_deg = frozenset(sorted_deg[:num_seeds])
+
+    # Compute sample space and probabilities for our baseline.
+    sample_nodes = sorted_deg[:(G.order() // 2)]
+    sample_probs = np.array([c_deg[node] for node in sample_nodes])
+    sample_probs = sample_probs / np.sum(sample_probs)
+    print(f'Meta seeds: {[idx_to_label[node] for node in seeds_deg]}')
+
+    with concurrent.futures.ThreadPoolExecutor(max_workers=num_workers) as monkey:
+        peels = [monkey.submit(banana, A, num_seeds, seeds_deg, sample_nodes, sample_probs, mins=mins) for _ in range(num_workers)]
+        concurrent.futures.wait(peels)
+    # banana(A, num_seeds, seeds_deg, sample_nodes, sample_probs)
+
+    # Export seed that works best.
+    if len(winners) > 0:
+        winner = max(winners, key=winners.get)
+    else:
+        winner = seeds_deg
+    winner_labels = [idx_to_label[node] for node in winner]
+    return [frozenset(winner_labels)]
+
+def banana(A, n_seeds, meta_seeds, sample_nodes, probs, mins=4):
+    start_time = datetime.now()
+    while (datetime.now() - start_time).total_seconds() < mins*60:
+        # print((datetime.now() - start_time).total_seconds())
+        seeds = frozenset(np.random.choice(sample_nodes, n_seeds, replace=False, p=probs))
+
+        if seeds not in tested:
+            tested.add(seeds)
+            monkey_score, machine_score = sim_1v1(A, seeds, meta_seeds)
+
+            if monkey_score > machine_score:
+                winners[seeds] = monkey_score
+                print(f'Monkey go brr! Seed: {seeds}, Score: {monkey_score}:{machine_score}')
 
 if __name__ == '__main__':
     main()
